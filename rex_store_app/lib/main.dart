e17778d1b2e1df70f8dfe4096cd3f975a21e5c87
YenHsinWu <rex19960408@gmail.com>
2025-10-26 14:51:40 +0800

    Add Splash Cubit for handling splash screen jump

--- rex_store_app/lib/main.dart
+++ rex_store_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rex_store_app/core/configs/theme/app_theme.dart';
+import 'package:rex_store_app/presentation/splash/bloc/splash_cubit.dart';
 import 'package:rex_store_app/presentation/splash/pages/splash_page.dart';
 
 void main() {
@@ -11,10 +13,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.appTheme,
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+    return BlocProvider(
+      create: (context) => SplashCubit()..appStarted(),
+      child: MaterialApp(
+        theme: AppTheme.appTheme,
+        debugShowCheckedModeBanner: false,
+        home: SplashPage(),
+      ),
     );
   }
 }
--- rex_store_app/lib/presentation/splash/pages/splash_page.dart
+++ rex_store_app/lib/presentation/splash/pages/splash_page.dart
@@ -1,5 +1,9 @@
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
 import "package:flutter_svg/flutter_svg.dart";
+import "package:rex_store_app/presentation/auth/pages/signin_page.dart";
+import "package:rex_store_app/presentation/splash/bloc/splash_cubit.dart";
+import "package:rex_store_app/presentation/splash/bloc/splash_state.dart";
 
 import "../../../core/configs/assets/app_vectors.dart";
 import "../../../core/configs/theme/app_colors.dart";
@@ -9,13 +13,21 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.primary,
-      body: Center(
-        child: SvgPicture.asset(
-          AppVectors.appLogo
+    return BlocListener<SplashCubit, SplashState>(
+      listener: (context, state) {
+        if (state is UnAuthenticated) {
+          Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SigninPage()));
+        }
+      },
+      child: Scaffold(
+        backgroundColor: AppColors.primary,
+        body: Center(
+          child: SvgPicture.asset(
+            AppVectors.appLogo
+          )
         )
-      )
+      ),
     );
   }
 }
