fe2f1e55fc975b614b431d2169bdd4a94d6fb1e3
YenHsinWu <rex19960408@gmail.com>
2025-12-04 17:01:37 +0800

    create enter password page

---
+++ rex_store_app/lib/presentation/auth/pages/enter_password_page.dart
@@ -0,0 +1,71 @@
+import "package:flutter/gestures.dart";
+import "package:flutter/material.dart";
+import "package:rex_store_app/common/widgets/button/basic_app_button.dart";
+
+class EnterPasswordPage extends StatelessWidget {
+  const EnterPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 80,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _signinText(context),
+            const SizedBox(height: 20,),
+            _passwordField(context),
+            const SizedBox(height: 20,),
+            _continueButton(),
+            const SizedBox(height: 20,),
+            _forgotPassword(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signinText(BuildContext context) {
+    return const Text(
+        'Sign In', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold));
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return const TextField(
+        decoration: InputDecoration(
+          hintText: "Enter Password",
+        )
+    );
+  }
+
+  Widget _continueButton() {
+    return BasicAppButton(
+      onPressed: () {},
+      title: "Continue",
+    );
+  }
+
+  Widget _forgotPassword(BuildContext context) {
+    return RichText(
+        text: TextSpan(
+            children: [
+              const TextSpan(
+                text: "Forgot password?",
+              ),
+              TextSpan(
+                  text: "Reset",
+                  recognizer: TapGestureRecognizer()..onTap = () {
+
+                  },
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )
+              )
+            ]
+        ));
+  }
+}
